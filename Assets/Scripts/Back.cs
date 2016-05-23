944a51681dad177cace7123a998e8af7903ba2bc
前原謙一 <junniku33@gmail.com>
2016-05-23 16:07:08 +0900

    scroll bg

---
+++ Assets/Scripts/Back.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Back : MonoBehaviour {
+private Camera _mainCamera;
+int xpos;
+float vel;
+float width;
+Vector3 bg_size;
+	// Use this for initialization
+	void Start () {
+		// SpriteをPrefabとしてLoad
+		GameObject prefab = Resources.Load<GameObject> ("Prefabs/Back");
+		SpriteRenderer renderer = prefab.GetComponent<SpriteRenderer>();
+		vel=0.01f; 
+		Vector3 bottomRight = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+    // 上下反転させる
+    	bottomRight.Scale(new Vector3(1f, -1f, 1f));
+    	width=bottomRight.x;
+    	bg_size=renderer.bounds.size;
+    	Debug.Log(bottomRight);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.x=pos.x+vel;
+		//Debug.Log(pos.x);
+		if(pos.x>width+bg_size.x/2){
+			Debug.Log(pos.x);
+			Vector3 TopLeft = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+    		// 上下反転させる
+    		TopLeft.Scale(new Vector3(1f, -1f, 1f));
+			pos.x=TopLeft.x-bg_size.x/2;
+		 }
+		transform.position=pos;
+	}
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+/// 敵
+public class Enemy : Token
+{
+  /// 開始
+  void Start()
+  {
+    // サイズを設定
+    SetSize(SpriteWidth / 2, SpriteHeight / 2);
+
+    // ランダムな方向に移動する
+    // 方向をランダムに決める
+    float dir = Random.Range(0, 359);
+    // 速さは2
+    float spd = 2;
+    SetVelocity(dir, spd);
+  }
+  void Update()
+  {
+  	if (Input.GetMouseButtonDown (0)) {
+        Vector2 tapPoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+        Collider2D collider = Physics2D.OverlapPoint (tapPoint);
+        if (collider != null) {
+            GameObject obj = collider.transform.gameObject;
+            Destroy (obj);
+        }
+    }
+    // カメラの左下座標を取得
+    Vector2 min = GetWorldMin();
+    // カメラの右上座標を取得する
+    Vector2 max = GetWorldMax();
+
+    if (X < min.x || max.x < X)
+    {
+      // 画面外に出たので、X移動量を反転する
+      VX *= -1;
+      // 画面内に移動する
+      ClampScreen();
+    }
+    if (Y < min.y || max.y < Y)
+    {
+      // 画面外に出たので、Y移動量を反転する
+      VY *= -1;
+      // 画面内に移動する
+      ClampScreen();
+    }
+  }
+  void onMouseDown(){
+  	Debug.Log("Click");
+  	DestroyObj();
+  }
+}
